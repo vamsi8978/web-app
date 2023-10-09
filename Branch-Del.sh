2cfebe2a3486b75a7f5b5e0e2d45438f7afae8f7
vamsi8978 <vamsichitturi007@gmail.com>
2023-10-09 12:42:22 -0400

    deletion of branches

---
+++ Branch-Del.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+REPO_OWNER="vamsi8978"
+REPO_NAME="web-app"
+
+GITHUB_TOKEN="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"
+
+DAYS_THRESHOLD=30
+
+CURRENT_DATE=$(10/9/2023 +%s)
+
+BRANCHES=$(curl -s -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/branches" | grep -o '"name": "[^"]*' | sed 's/"name": "//')
+
+for BRANCH in $BRANCHES; do
+    LAST_COMMIT_DATE=$(git log -1 --format=%ct origin/$BRANCH 2>/dev/null)
+
+    if [ -n "$LAST_COMMIT_DATE" ]; then
+        DAYS_SINCE_LAST_COMMIT=$((($CURRENT_DATE - $LAST_COMMIT_DATE) / 86400))
+
+        if [ $DAYS_SINCE_LAST_COMMIT -gt $DAYS_THRESHOLD ]; then
+            echo "Deleting branch: $BRANCH"
+            curl -X DELETE -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/git/refs/heads/$BRANCH"
+        fi
+    fi
+done
+
+echo "Branches older than $DAYS_THRESHOLD days deleted successfully."
+
